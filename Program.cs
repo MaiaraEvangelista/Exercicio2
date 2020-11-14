c0a38d3f3679d81743851296a91b57fd1918fb75
Maiara Evangelista <maiaraevangelista933@gmail.com>
2020-11-14 16:31:49 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano atual: ");
+            double data1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua data de nascimento: ");
+            double data2 = double.Parse(Console.ReadLine());
+
+            double resultado = data1 - data2;
+
+            Console.WriteLine(resultado);
+
+            Console.WriteLine("Converta sua idade em semanas, digite aqui sua idade: ");
+            int conversao = int.Parse(Console.ReadLine());
+            Console.WriteLine(resultado*48);
+            
+        }
+    }
+}
